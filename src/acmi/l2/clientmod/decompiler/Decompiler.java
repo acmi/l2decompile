0653ed2b0f00549d48162345c10e866e809b25b4
Aleksei Sazonov <s.aleksey@russianit.ru>
2016-05-10 17:09:34 +0400

    lib update

--- src/acmi/l2/clientmod/decompiler/Decompiler.java
+++ src/acmi/l2/clientmod/decompiler/Decompiler.java
@@ -21,41 +21,28 @@
  */
 package acmi.l2.clientmod.decompiler;
 
-import acmi.l2.clientmod.io.UnrealPackageReadOnly;
+import acmi.l2.clientmod.io.UnrealPackage;
+import acmi.l2.clientmod.unreal.UnrealSerializerFactory;
 import acmi.l2.clientmod.unreal.core.*;
 import acmi.l2.clientmod.unreal.core.Class;
 import acmi.l2.clientmod.unreal.core.Enum;
-import acmi.l2.clientmod.unreal.core.Function;
-import acmi.l2.clientmod.unreal.core.Object;
-import acmi.l2.clientmod.unreal.objectfactory.ObjectFactory;
 import javafx.util.Pair;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static acmi.l2.clientmod.decompiler.Util.*;
+import static acmi.l2.clientmod.decompiler.Util.newLine;
 
 public class Decompiler {
-    static Object instantiate(UnrealPackageReadOnly.ExportEntry entry, ObjectFactory objectFactory) {
-        String objClass = entry.getObjectClass() == null ? "Core.Class" : entry.getObjectClass().getObjectFullName();
-        if (objClass.equals("Core.Class") ||
-                objClass.equals("Core.State") ||
-                objClass.equals("Core.Function") ||
-                objClass.equals("Core.Struct")) {
-            return objectFactory.getClassLoader().getStruct(entry.getObjectFullName());
-        } else {
-            return objectFactory.apply(entry);
-        }
-    }
-
-    public static CharSequence decompile(Class clazz, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompile(Class clazz, UnrealSerializerFactory objectFactory, int indent) {
         StringBuilder sb = new StringBuilder();
 
-        String name = clazz.getEntry().getObjectName().getName();
-        String superName = clazz.getEntry().getObjectSuperClass() != null ?
-                clazz.getEntry().getObjectSuperClass().getObjectName().getName() : null;
+        String name = clazz.entry.getObjectName().getName();
+        String superName = clazz.entry.getObjectSuperClass() != null ?
+                clazz.entry.getObjectSuperClass().getObjectName().getName() : null;
 
         sb.append("class ").append(name);
         if (superName != null)
@@ -63,7 +50,7 @@ public class Decompiler {
         //TODO flags
         sb.append(";");
 
-        if (clazz.getChild() != null) {
+        if (clazz.child != null) {
             sb.append(newLine());
             sb.append(newLine(indent)).append(decompileFields(clazz, objectFactory, indent));
         }
@@ -73,10 +60,10 @@ public class Decompiler {
         return sb;
     }
 
-    public static CharSequence decompileFields(Struct struct, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompileFields(Struct struct, UnrealSerializerFactory objectFactory, int indent) {
         Stream.Builder<CharSequence> fields = Stream.builder();
 
-        for (Field field : (Iterable<Field>) () -> new ChildIterator(struct, objectFactory)) {
+        struct.forEach(field -> {
             if (field instanceof Const) {
                 fields.add(decompileConst((Const) field, objectFactory, indent) + ";");
             } else if (field instanceof Enum) {
@@ -84,7 +71,7 @@ public class Decompiler {
                     fields.add(decompileEnum((Enum) field, objectFactory, indent) + ";");
             } else if (field instanceof Property) {
                 if (field instanceof DelegateProperty)
-                    continue;
+                    return;
 
                 fields.add(decompileProperty((Property) field, struct, objectFactory, indent) + ";");
             } else if (field instanceof State) {
@@ -96,51 +83,48 @@ public class Decompiler {
             } else {
                 fields.add(field.toString());
             }
-        }
+        });
 
         return fields.build().collect(Collectors.joining(newLine(indent)));
     }
 
-    public static CharSequence decompileConst(Const c, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompileConst(Const c, UnrealSerializerFactory objectFactory, int indent) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("const ")
-                .append(c.getEntry().getObjectName().getName())
+                .append(c.entry.getObjectName().getName())
                 .append(" =")
-                .append(c.constant);
+                .append(c.value);
 
         return sb;
     }
 
-    public static CharSequence decompileEnum(Enum e, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompileEnum(Enum e, UnrealSerializerFactory objectFactory, int indent) {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("enum ").append(e.getEntry().getObjectName().getName())
+        sb.append("enum ").append(e.entry.getObjectName().getName())
                 .append(newLine(indent)).append("{")
-                .append(newLine(indent + 1)).append(e.getValues().stream().collect(Collectors.joining("," + newLine(indent + 1))))
+                .append(newLine(indent + 1)).append(Arrays.stream(e.values).collect(Collectors.joining("," + newLine(indent + 1))))
                 .append(newLine(indent)).append("}");
 
         return sb;
     }
 
-    public static CharSequence decompileProperty(Property property, Struct parent, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompileProperty(Property property, Struct parent, UnrealSerializerFactory objectFactory, int indent) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("var");
         CharSequence type = getType(property, objectFactory, true);
         if (parent.getClass().equals(Struct.class)) {
             if (property instanceof ByteProperty &&
-                    ((ByteProperty) property).enumType != 0) {
-                UnrealPackageReadOnly.Entry enumLocalEntry = ((ByteProperty) property).getEnumType();
-                UnrealPackageReadOnly.ExportEntry enumEntry = objectFactory.getClassLoader()
-                        .getExportEntry(enumLocalEntry.getObjectFullName(), e -> e.getObjectClass() != null && e.getObjectClass().getObjectFullName().equalsIgnoreCase("Core.Enum"));
-                Enum en = (Enum) objectFactory.apply(enumEntry);
+                    ((ByteProperty) property).enumType != null) {
+                Enum en = ((ByteProperty) property).enumType;
                 type = decompileEnum(en, objectFactory, indent);
             }
             //FIXME array<enum>
         }
         sb.append(" ").append(type).append(" ");
-        sb.append(property.getEntry().getObjectName().getName());
+        sb.append(property.entry.getObjectName().getName());
         if (property.arrayDimension > 1)
             sb.append("[").append(property.arrayDimension).append("]");
 
@@ -148,30 +132,30 @@ public class Decompiler {
     }
 
     private static final List<Pair<Predicate<Property>, java.util.function.Function<Property, String>>> MODIFIERS = Arrays.asList(
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Edit), p -> "(" + (p.getEntry().getObjectPackage().getObjectName().getName().equalsIgnoreCase(p.getCategory()) ? "" : p.getCategory()) + ")"),
-            new Pair<>(p -> UnrealPackageReadOnly.ObjectFlag.getFlags(p.getEntry().getObjectFlags()).contains(UnrealPackageReadOnly.ObjectFlag.Private), p -> "private"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Const), p -> "const"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Input), p -> "input"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.ExportObject), p -> "export"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.OptionalParm), p -> "optional"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.OutParm), p -> "out"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.SkipParm), p -> "skip"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.CoerceParm), p -> "coerce"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Native), p -> "native"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Transient), p -> "transient"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Config), p -> p.getPropertyFlags().contains(Property.CPF.GlobalConfig) ? null : "config"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Localized), p -> "localized"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Travel), p -> "travel"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.EditConst), p -> "editconst"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.GlobalConfig), p -> "globalconfig"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.EditInline), p -> p.getPropertyFlags().contains(Property.CPF.EditInlineUse) ? null : "editinline"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.EdFindable), p -> "edfindable"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.EditInlineUse), p -> "editinlineuse"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.Deprecated), p -> "deprecated"),
-            new Pair<>(p -> p.getPropertyFlags().contains(Property.CPF.EditInlineNotify), p -> "editinlinenotify")
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Edit), p -> "(" + (p.entry.getObjectPackage().getObjectName().getName().equalsIgnoreCase(p.category) ? "" : p.category) + ")"),
+            new Pair<>(p -> UnrealPackage.ObjectFlag.getFlags(p.entry.getObjectFlags()).contains(UnrealPackage.ObjectFlag.Private), p -> "private"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Const), p -> "const"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Input), p -> "input"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.ExportObject), p -> "export"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.OptionalParm), p -> "optional"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.OutParm), p -> "out"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.SkipParm), p -> "skip"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.CoerceParm), p -> "coerce"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Native), p -> "native"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Transient), p -> "transient"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Config), p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.GlobalConfig) ? null : "config"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Localized), p -> "localized"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Travel), p -> "travel"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.EditConst), p -> "editconst"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.GlobalConfig), p -> "globalconfig"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.EditInline), p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.EditInlineUse) ? null : "editinline"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.EdFindable), p -> "edfindable"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.EditInlineUse), p -> "editinlineuse"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.Deprecated), p -> "deprecated"),
+            new Pair<>(p -> Property.CPF.getFlags(p.propertyFlags).contains(Property.CPF.EditInlineNotify), p -> "editinlinenotify")
     );
 
-    public static CharSequence getType(Property property, ObjectFactory objectFactory, boolean includeModifiers) {
+    public static CharSequence getType(Property property, UnrealSerializerFactory objectFactory, boolean includeModifiers) {
         StringBuilder sb = new StringBuilder();
 
         if (includeModifiers) {
@@ -181,8 +165,8 @@ public class Decompiler {
                     .forEach(m -> sb.append(m).append(" "));
         }
         if (property instanceof ByteProperty) {
-            if (((ByteProperty) property).enumType != 0) {
-                UnrealPackageReadOnly.Entry enumLocalEntry = ((ByteProperty) property).getEnumType();
+            if (((ByteProperty) property).enumType != null) {
+                UnrealPackage.Entry enumLocalEntry = ((ByteProperty) property).enumType.entry;
                 sb.append(enumLocalEntry.getObjectName().getName());
             } else {
                 sb.append("byte");
@@ -194,15 +178,15 @@ public class Decompiler {
         } else if (property instanceof FloatProperty) {
             sb.append("float");
         } else if (property instanceof ObjectProperty) {
-            sb.append(((ObjectProperty) property).getObjectType().getObjectName().getName());
+            sb.append(((ObjectProperty) property).type.entry.getObjectName().getName());
         } else if (property instanceof NameProperty) {
             sb.append("name");
         } else if (property instanceof ArrayProperty) {
             ArrayProperty arrayProperty = (ArrayProperty) property;
-            Property innerProperty = (Property) objectFactory.apply(objectFactory.getClassLoader().getExportEntry(arrayProperty.getInner().getObjectFullName(), e -> true));
+            Property innerProperty = arrayProperty.inner;
             sb.append("array<").append(getType(innerProperty, objectFactory, false)).append(">");
         } else if (property instanceof StructProperty) {
-            sb.append(((StructProperty) property).getStructType().getObjectName().getName());
+            sb.append(((StructProperty) property).struct.entry.getObjectName().getName());
         } else if (property instanceof StrProperty) {
             sb.append("string");
         }
@@ -210,30 +194,30 @@ public class Decompiler {
         return sb;
     }
 
-    public static CharSequence decompileStruct(Struct struct, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompileStruct(Struct struct, UnrealSerializerFactory objectFactory, int indent) {
         StringBuilder sb = new StringBuilder();
-        sb.append("struct ").append(struct.getEntry().getObjectName().getName());
+        sb.append("struct ").append(struct.entry.getObjectName().getName());
         sb.append(newLine(indent)).append("{");
         sb.append(newLine(indent + 1)).append(decompileFields(struct, objectFactory, indent + 1));
         sb.append(newLine(indent)).append("}");
         return sb;
     }
 
-    public static CharSequence decompileFunction(Function function, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompileFunction(Function function, UnrealSerializerFactory objectFactory, int indent) {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("//function_").append(function.getFriendlyName()); //TODO
+        sb.append("//function_").append(function.friendlyName); //TODO
 
         return sb;
     }
 
-    public static CharSequence decompileState(State state, ObjectFactory objectFactory, int indent) {
+    public static CharSequence decompileState(State state, UnrealSerializerFactory objectFactory, int indent) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("state ");
-        sb.append(state.getEntry().getObjectName().getName());
-        if (state.getEntry().getObjectSuperClass() != null) {
-            sb.append(" extends ").append(state.getEntry().getObjectSuperClass().getObjectName().getName());
+        sb.append(state.entry.getObjectName().getName());
+        if (state.entry.getObjectSuperClass() != null) {
+            sb.append(" extends ").append(state.entry.getObjectSuperClass().getObjectName().getName());
         }
         sb.append(newLine(indent)).append("{");
         sb.append(newLine(indent + 1)).append(decompileFields(state, objectFactory, indent + 1));
